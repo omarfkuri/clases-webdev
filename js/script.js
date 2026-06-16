44ab4509bfdb6f229f667ce3018eb4d829e13de2
Omar <omarfkuri@gmail.com>
2026-06-16 02:06:34 -0600

    Parte 16

--- js/script.js
+++ js/script.js
@@ -1,59 +1,24 @@
-// El código definido normalmente
-// se ejecuta secuencialmente
-
-let x = 0;
-
-x = 5;
-console.log(x);
-
-x = 7;
-console.log(x);
-
-
-// API de promesas
+// Al definir el script como type="module"
+// se habilita la palabra `await`. Esta permite
+// esperar a que una promesa se complete,
+// lo cual permite escribir código que puede tomar tiempo
+// de una manera secuencial.
 
 const api = 'https://jsonplaceholder.typicode.com/todos/1';
 
-// Se buscan datos en un servidor externo.
-// Se recibe una promesa, que promete regresar
-// datos eventualmente.
-fetch(api)
-// Se crea una microtarea y cuando se completa
-// se ejecuta el método `then`.
-.then(res => {
+try 
+{
+	// Respuesta no contiene una promesa, si
+	// no el resultado de la misma
+	const respuesta = await fetch(api);
 
-	// Se obtiene una respuesta del servidor
-	// con contenido sin analizar.
+	// El objeto aquí ya está analizado
+	const obj = await respuesta.json();
 
-	// Se hace una nueva promesa que promete
-	// analizar el contenido y convertir a
-	// un objeto.
-	return res.json();
-})
-.then(obj => {
-	// Se completa la segunda promesa.
-
-	// Este objeto contiene el resultado.
 	console.log(obj);
-})
-.catch(error => {
-	// Si ocurre un error en cualquier punto,
-	// se ejecuta esta función.
-
-	console.log(error);
-})
-
-
-setTimeout(() => {
-	// La ejecución de esta función
-	// se pasa a una microtarea
-
-	// Esta función se ejecutará
-	// después del tiempo pasado
-	// como segundo parámetro.
-
-	console.log("Pasaron 4s");
-}, 4000)
-
-// Esto se ejecutará antes de fetch y setTimeout
-console.log("Antes de fetch y setTimeout");
+}
+// Se ejecuta si ocurre un error
+catch(err)
+{
+	console.log(err);
+}
