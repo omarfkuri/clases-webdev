160b6bd7cf5a5d35032b33a40455cc19292d30bb
Omar <omarfkuri@gmail.com>
2026-06-16 02:04:04 -0600

    Parte 15

--- js/script.js
+++ js/script.js
@@ -1,79 +1,59 @@
-class Persona
-{
-	constructor(nombre, apellido)
-	{
-		this.nombre = nombre;
-		this.apellido = apellido;
-	}
+// El código definido normalmente
+// se ejecuta secuencialmente
 
-	// Método de instancia
-	decirHola()
-	{
-		console.log(this.nombre, "dice hola")
-	}
+let x = 0;
 
-	// Método de clase
-	static decirAdios()
-	{
-		console.log("Adiós");
-	}
-}
+x = 5;
+console.log(x);
 
-// Para crear instancias de clase, utilizar new
-const dan = new Persona("Daniel", "De La Cruz");
-console.log(dan);
-dan.decirHola();
-
-Persona.decirAdios();
-
-class Empleado extends Persona
-{
-	// Variable de instancia
-	id = "jkahslkfhas"
-
-	// Solo accesible dentro de la clase
-	#priv = 333;
-
-	// Si no se define constructor,
-	// se hereda el del padre
-	constructor(id, nombre, apellido)
-	{
-		super(nombre, apellido)
-
-		// Si no se asigna el valor,
-		// se queda el asignado anteriormente
-		this.id = id;
-	}
-
-	decirCodigoPrivado()
-	{
-		console.log(this.#priv);
-	}
-}
-
-const luis = new Empleado(893257, "luis", "rodriguez");
-console.log(luis);
-luis.decirCodigoPrivado();
-
-// Siendo objetos, se pueden agregar propiedades
-luis.colonia = "Malamuerte (Santocho)";
-console.log(luis);
+x = 7;
+console.log(x);
 
 
-// Las clases son azúcar sintáctica para:
+// API de promesas
 
-function PersonFun(nombre, apellido)
-{
-	return {
-		nombre: nombre,
-		apellido: apellido,
+const api = 'https://jsonplaceholder.typicode.com/todos/1';
 
-		decirHola()
-		{
-			console.log(this.nombre, "dice hola");
-		}
-	}
-}
+// Se buscan datos en un servidor externo.
+// Se recibe una promesa, que promete regresar
+// datos eventualmente.
+fetch(api)
+// Se crea una microtarea y cuando se completa
+// se ejecuta el método `then`.
+.then(res => {
 
-const per = PersonFun("Marco", "Martínez");
-console.log(per);
+	// Se obtiene una respuesta del servidor
+	// con contenido sin analizar.
+
+	// Se hace una nueva promesa que promete
+	// analizar el contenido y convertir a
+	// un objeto.
+	return res.json();
+})
+.then(obj => {
+	// Se completa la segunda promesa.
+
+	// Este objeto contiene el resultado.
+	console.log(obj);
+})
+.catch(error => {
+	// Si ocurre un error en cualquier punto,
+	// se ejecuta esta función.
+
+	console.log(error);
+})
+
+
+setTimeout(() => {
+	// La ejecución de esta función
+	// se pasa a una microtarea
+
+	// Esta función se ejecutará
+	// después del tiempo pasado
+	// como segundo parámetro.
+
+	console.log("Pasaron 4s");
+}, 4000)
+
+// Esto se ejecutará antes de fetch y setTimeout
+console.log("Antes de fetch y setTimeout");
