745aad1723a8a899cfff221b90414bd9467139f7
Omar <omarfkuri@gmail.com>
2026-06-16 02:12:59 -0600

    Parte 18

--- js/script.js
+++ js/script.js
@@ -1,35 +1,72 @@
-// window: Window
-// Este objeto contiene todos
-// los objetos disponibles en el API
-// de la web.
-console.log(window);
+// Manipulación del DOM
 
-// No se tiene que referenciar
-// window, pues se tiene acceso a todas las
-// props de window directamente.
-console.log(alert, window.alert);
+// Selección de elementos (se pueden usar selectores
+// como en CSS)
+const elemPag = document.querySelector("header h1");
+console.log(elemPag)
 
-// DOM
-// Document Object Model
-// Este modelo representa el árbol de contenido
-// dentro de la página web.
-console.log(document, window.document);
+// Crear elementos
+const miElem = document.createElement("input");
+console.log(miElem)
 
-// TODO es un NODO (Node)
-// y el DOM está compuesto de ellos.
-console.log(Node);
+// Definir atributos
+miElem.type = "password";
+miElem.name = "mi-contraseña"
 
-/*
-Jerarquía de clases
+const miPadre = document.createElement("div");
+miPadre.id = "padre";
 
-Node
-	Text
-	Element
+// Agregar hijos a un elemento
+for (let i = 0; i < 10; i++)
+{
+	const miHijo = document.createElement("h2");
 
-Element
-	HTMLElement
-		HTMLDivElement
-		HTMLAnchorElement
-		HTMLHeadingElement
-*/
+	miHijo.className = "hijo-" + i;
+	miHijo.innerText = String(i);
+	miPadre.appendChild(miHijo);
+}
 
+// Este elemento aún no es parte del dom
+console.log(miPadre);
+
+// Al buscarlo en el dom, no lo encontraremos
+console.log(document.querySelector("#padre"));
+
+// Se busca un hijo dentro del padre
+const hijo6 = miPadre.querySelector(".hijo-6");
+console.log(hijo6);
+
+// Se remueve ese hijo del padre
+miPadre.removeChild(hijo6);
+// También:
+// hijo6.remove();
+console.log(miPadre);
+
+// Agregar al DOM (al afectar el DOM se 
+// observan los cambios de inmediato)
+const he = document.querySelector("header");
+he.appendChild(miPadre)
+
+// actualizar elementos
+const hijo7 = miPadre.querySelector(".hijo-7");
+hijo7.innerText = "Jajaja";
+
+// Alterar CSS
+hijo7.style = "background-color: red;";
+hijo7.style.color = "white";
+
+const b = document.querySelector("body");
+
+b.style.display = "flex";
+b.style.flexDirection = "column";
+b.style.border = "1px solid red"
+
+// Agregar nueva hoja de estilos
+const s = document.createElement("style")
+s.innerText = "body { background-color: blue }";
+document.body.appendChild(s);
+
+// Agregar nueva script
+const s2 = document.createElement("script")
+s2.innerText = "console.log('hola')";
+document.body.appendChild(s2);
